9d8345fd03eff2520e7a2edcf2af59bb412d41a4
kosei0227 <167411842+kosei0227@users.noreply.github.com>
2024-10-24 00:27:24 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+ <html lang="ja">
+
+ <head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+ </head>
+ <body>
+    <p>
+     <?php
+      $about_onion=['name'=>'onion','price'=>200,'weight'=>160];
+      print_r($about_onion);
+     ?>
+    </p>
+ </body>
+</html>
